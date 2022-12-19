b4c66961712e7f9fcf0be64984c645b2c0bc9728
Chirag Goel <chirag.goel@altudo.co>
2022-12-19 13:51:54 +0530

    Added Visitor Identification Model View and Controller

---
+++ SitecoreCDP/src/Feature/Components/code/Controllers/CDPIdentificationController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SitecoreCDP.Feature.Components.Models;
+
+namespace SitecoreCDP.Feature.Components.Controllers
+{
+    public class CDPIdentificationController : Controller
+    {
+        public ActionResult GetIdentification()
+        {
+            VisitorInformation visitorInformation = new VisitorInformation();
+            return View(visitorInformation);
+        }
+    }
+}
---
+++ SitecoreCDP/src/Feature/Components/code/Models/VisitiorInformation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SitecoreCDP.Feature.Components.Models
+{
+    public class VisitorInformation
+    {
+        public string FullName { get; set; }
+        public string Gender { get; set; }
+        public string Nationality { get; set; }
+        public string DOB { get; set; }
+        public string Email { get; set; }
+        public string PhoneNo { get; set; }
+    }
+}
