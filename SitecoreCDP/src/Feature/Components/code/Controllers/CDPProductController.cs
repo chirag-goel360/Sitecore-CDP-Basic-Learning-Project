4b497fd1996bd010334da18e0d5547888b14d2f7
Chirag Goel <chirag.goel@altudo.co>
2022-12-21 16:53:46 +0530

    Product Component

---
+++ SitecoreCDP/src/Feature/Components/code/Controllers/CDPProductController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SitecoreCDP.Feature.Components.Controllers
+{
+    public class CDPProductController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
