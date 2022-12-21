4355d3c517461668b63fdb24e9dd82b196814415
Chirag Goel <chirag.goel@altudo.co>
2022-12-21 18:20:24 +0530

    Added Boxever Script

---
+++ SitecoreCDP/src/Project/Website/code/Constants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SitecoreCDP.Project.Website
+{
+    public class Constants
+    {
+        public const string client_key = "";
+        public const string targetEndPoint = "https://api.boxever.com/v1.2";
+        public const string cookieDomain = ".sitecorecdp.dev.local";
+        public const string javascriptLibraryVersion = "1.4.9";
+        public const string pointOfSale = "";
+    }
+}
